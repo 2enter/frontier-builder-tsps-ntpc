0c240a1f4e87ea7159cc71588d1e68eeae100529
davilivob <david.iii.chiu@gmail.com>
2026-03-04 20:15:34 +0800

    [frontend] enhance input state handling and improve page interactions

--- frontend/src/routes/app/pages/intro.svelte
+++ frontend/src/routes/app/pages/intro.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	import { ImgBtn } from '@2enter/web-kit/components';
-	import { getSysState } from '@/states';
+    import {ImgBtn} from '@2enter/web-kit/components';
+    import {getSysState, getInputState} from '@/states';
 
-	const sysState = getSysState();
+    const sysState = getSysState();
+    const inputState = getInputState();
+
 </script>
 
-<img class="pointer-events-none fixed h-full" src="/ui/layouts/intro_page.webp" alt="" />
-<ImgBtn src="/ui/buttons/start.webp" class="fixed bottom-[30vh] w-1/2" onclick={sysState.navigate}></ImgBtn>
+<img class="pointer-events-none fixed h-full" src="/ui/layouts/intro_page.webp" alt=""/>
+<ImgBtn src="/ui/buttons/start.webp" class="fixed bottom-[30vh] w-1/2" onclick={sysState.navigate}
+></ImgBtn>
+
+<div
+        class="fixed bottom-0 left-0 size-3"
+        aria-label="Double click to configure"
+        role="button"
+        tabindex="0"
+        ondblclick={inputState.goToConfig}
+></div>
--- frontend/src/routes/app/pages/select.svelte
+++ frontend/src/routes/app/pages/select.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
     import {CargoKind} from '@/types/model';
 
-    import {getInputState, getSysState} from '@/states';
+    import {getSysState} from '@/states';
     import {ImgBtn} from '@2enter/web-kit/components';
 
-    const [inputState, sysState] = [getInputState(), getSysState()];
+    const sysState = getSysState();
     const CARGO_TYPES = Object.values(CargoKind);
 
-    function choose(kind: CargoKind) {
-        inputState.cargoKind = kind;
+    function choose() {
         sysState.navigate();
     }
 </script>
 
 <div class="center-content flex-wrap gap-3 px-14">
-    <img src="/ui/texts/select_cargo.webp" alt=""/>
+    <img src="/ui/texts/select_cargo.webp" alt="" onclick={choose}/>
     {#each CARGO_TYPES as type}
         <ImgBtn src="/cargoes/{type}_select.webp" class="w-2/5" onclick={() => choose(type)}></ImgBtn>
     {/each}
