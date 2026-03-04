9801a6960f4d5b5e21f8ee813992314b657ef4e9
davilivob <david.iii.chiu@gmail.com>
2026-03-04 16:20:47 +0800

    [frontend] update model.ts

--- frontend/src/lib/types/model.ts
+++ frontend/src/lib/types/model.ts
@@ -33,3 +33,22 @@ export interface CargoRequest {
 	paintTime: number;
 }
 
+/**
+ * Custom JSON reviver and replacer functions for dynamic data transformation
+ * ReviverFunc is used during JSON parsing to detect and transform specific data structures
+ * ReplacerFunc is used during JSON serialization to modify certain values before stringifying.
+ * These functions allow for flexible encoding and decoding of data, ensuring that complex types are properly handled when converting between TS objects and JSON
+ */
+export const ReviverFunc = (key: string, value: unknown): unknown => {
+    if (typeof value === "string" && /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z$/.test(value) && (key === "createdAt")) {
+        return new Date(value);
+    }
+    return value;
+};
+
+export const ReplacerFunc = (key: string, value: unknown): unknown => {
+    if (value instanceof Date) {
+        return value.toISOString();
+    }
+    return value;
+};
