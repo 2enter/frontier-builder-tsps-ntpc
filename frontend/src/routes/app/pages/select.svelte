94a6d3c137418ea8afe3e61d8a4e41c9fc533ba0
davilivob <david.iii.chiu@gmail.com>
2026-03-11 16:44:11 +0800

    [frontend] update select page

--- frontend/src/routes/app/pages/select.svelte
+++ frontend/src/routes/app/pages/select.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import {CargoKind} from '@/types/model';
 
-    import {getSysState} from '@/states';
+    import {getSysState, getInputState} from '@/states';
     import {ImgBtn} from '@2enter/web-kit/components';
 
-    const sysState = getSysState();
-    const CARGO_TYPES = Object.values(CargoKind);
+    const [sysState, inputState] = [getSysState(), getInputState()];
+
+    const cargoKind = $derived(inputState.cargoKind);
+
 
     function choose() {
         sysState.navigate();
@@ -13,8 +15,11 @@
 </script>
 
 <div class="center-content flex-wrap gap-3 px-14">
-    <img src="/ui/texts/select_cargo.webp" alt="" onclick={choose}/>
-    {#each CARGO_TYPES as type}
-        <ImgBtn src="/cargoes/{type}_select.webp" class="w-2/5" onclick={() => choose(type)}></ImgBtn>
-    {/each}
+    <!--    <img src="/ui/texts/select_cargo.webp" alt="" onclick={choose}/>-->
+    <!--    {#each CARGO_TYPES as type}-->
+    <ImgBtn src="/cargoes/{cargoKind}_select.webp" class="w-2/5" onclick={ choose}></ImgBtn>
+    <img src="/cargoes/{cargoKind}_description.webp" alt="" />
+
+
+    <!--{/each}-->
 </div>
