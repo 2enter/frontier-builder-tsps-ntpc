188142fe702eb4b3640b4646aa67dad48f819620
davilivob <david.iii.chiu@gmail.com>
2026-03-11 16:43:25 +0800

    [frontend] update intro_page bg method

--- frontend/src/routes/app/pages/intro.svelte
+++ frontend/src/routes/app/pages/intro.svelte
@@ -7,7 +7,7 @@
 
 </script>
 
-<img class="pointer-events-none fixed h-full" src="/ui/layouts/intro_page.webp" alt=""/>
+<img class="pointer-events-none fixed w-full" src="/ui/layouts/intro_page.webp" alt=""/>
 <ImgBtn src="/ui/buttons/start.webp" class="fixed bottom-[30vh] w-1/2" onclick={sysState.navigate}
 ></ImgBtn>
 
